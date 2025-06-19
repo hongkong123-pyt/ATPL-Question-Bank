3d9476ada5eee63914c0d874c8f80cd64253a871
HongKong <djunavos@hotmail.com>
2025-06-19 22:56:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function checkPassword() {
+  const correctPassword = "salmari";
+  const input = document.getElementById("accessInput").value;
+  const error = document.getElementById("errorMsg");
+
+  if (input === correctPassword) {
+    document.getElementById("lockScreen").style.display = "none";
+    document.getElementById("quizApp").style.display = "block";
+  } else {
+    error.textContent = "Incorrect password. Try again.";
+  }
+}
+
 let data = {};
 let currentIndex = 0;
 let currentSubject = "";
